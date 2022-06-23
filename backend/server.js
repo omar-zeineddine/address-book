63f95fc65f7c7736dee60300f7097584e3980b77
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-06-23 23:54:13 +0300

    feat - add api route definitions

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the Address Book API" })
 );
 
+// Route definitions
+
+app.use("api/auth/", require("./routes/auth"));
+app.use("api/users/", require("./routes/users"));
+app.use("api/contacts/", require("./routes/contacts"));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

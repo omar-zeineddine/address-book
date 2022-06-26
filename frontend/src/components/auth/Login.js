7cdac1fc69b39308a6a4d1e16c637be6a3f84ccb
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-06-27 02:33:04 +0300

    fix - refactor components to display alerts

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+import AlertContext from '../../context/alert/alertContext';
+import { useAuth, clearErrors, login } from '../../context/auth/AuthState';
 
 const Login = () => {
+  const alertContext = useContext(AlertContext);
+  const { setAlert } = alertContext;
+
+  const [authState, authDispatch] = useAuth();
+  const { error, isAuthenticated } = authState;
+
+  useEffect(() => {
+    if (error === 'Invalid Credentials') {
+      setAlert(error, 'danger');
+      clearErrors(authDispatch);
+    }
+  }, [error, isAuthenticated, authDispatch, setAlert]);
+
   const [user, setUser] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: ''
   });
 
   const { email, password } = user;
@@ -12,41 +28,49 @@ const Login = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("login submit");
+    if (email === '' || password === '') {
+      setAlert('Please fill in all fields', 'danger');
+    } else {
+      login(authDispatch, {
+        email,
+        password
+      });
+    }
   };
+  if (isAuthenticated) return <Navigate to='/' />;
 
   return (
-    <div className="form-container">
+    <div className='form-container'>
       <h1>
-        Account <span className="text-primary">Login</span>
+        Account <span className='text-primary'>Login</span>
       </h1>
       <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email Address</label>
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address</label>
           <input
-            id="email"
-            type="email"
-            name="email"
+            id='email'
+            type='email'
+            name='email'
             value={email}
             onChange={onChange}
             required
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
           <input
-            id="password"
-            type="password"
-            name="password"
+            id='password'
+            type='password'
+            name='password'
             value={password}
             onChange={onChange}
             required
           />
         </div>
         <input
-          type="submit"
-          value="Login"
-          className="btn btn-primary btn-block"
+          type='submit'
+          value='Login'
+          className='btn btn-primary btn-block'
         />
       </form>
     </div>
--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+import AlertContext from '../../context/alert/alertContext';
+import { useAuth, clearErrors, register } from '../../context/auth/AuthState';
 
 const Register = (props) => {
+  const alertContext = useContext(AlertContext);
+  const [authState, authDispatch] = useAuth();
+  const { error, isAuthenticated } = authState;
+
+  const { setAlert } = alertContext;
+
+  useEffect(() => {
+    if (error === 'User already exists') {
+      setAlert(error, 'danger');
+      clearErrors(authDispatch);
+    }
+  }, [error, isAuthenticated, props.history, setAlert, authDispatch]);
+
   const [user, setUser] = useState({
-    name: "",
-    email: "",
-    password: "",
-    password2: "",
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
   });
 
   const { name, email, password, password2 } = user;
@@ -14,65 +30,77 @@ const Register = (props) => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("register submit");
+    if (name === '' || email === '' || password === '') {
+      setAlert('Please enter all fields', 'danger');
+    } else if (password !== password2) {
+      setAlert('Passwords do not match', 'danger');
+    } else {
+      register(authDispatch, {
+        name,
+        email,
+        password
+      });
+    }
   };
 
+  if (isAuthenticated) return <Navigate to='/' />;
+
   return (
-    <div className="form-container">
+    <div className='form-container'>
       <h1>
-        Account <span className="text-primary">Register</span>
+        Account <span className='text-primary'>Register</span>
       </h1>
       <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
           <input
-            id="name"
-            type="text"
-            name="name"
+            id='name'
+            type='text'
+            name='name'
             value={name}
             onChange={onChange}
             required
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Email Address</label>
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address</label>
           <input
-            id="email"
-            type="email"
-            name="email"
+            id='email'
+            type='email'
+            name='email'
             value={email}
             onChange={onChange}
             required
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
           <input
-            id="password"
-            type="password"
-            name="password"
+            id='password'
+            type='password'
+            name='password'
             value={password}
             onChange={onChange}
             required
-            minLength="6"
+            minLength='6'
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="password2">Confirm Password</label>
+        <div className='form-group'>
+          <label htmlFor='password2'>Confirm Password</label>
           <input
-            id="password2"
-            type="password"
-            name="password2"
+            id='password2'
+            type='password'
+            name='password2'
             value={password2}
             onChange={onChange}
             required
-            minLength="6"
+            minLength='6'
           />
         </div>
         <input
-          type="submit"
-          value="Register"
-          className="btn btn-primary btn-block"
+          type='submit'
+          value='Register'
+          className='btn btn-primary btn-block'
         />
       </form>
     </div>
